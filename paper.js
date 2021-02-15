2d0d89a285134987a56ca0c97e1c03580fc2fd85
aaryanagrawal46 <75481813+aaryanagrawal46@users.noreply.github.com>
2021-02-15 20:14:58 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class paper 
+{
+constructor(x,y,z)
+{
+var options{
+    isStaticfalse:
+    restitution:0.3,
+friction:0.5,
+density:1.2
+    
+}
+this.x;
+this.y;
+this.z;
+
+this.body.Bodies.circle(this.x,this.y,this.r,options)
+World.add(world,this.body);
+}
+display()
+{
+    var paperposition.body.position;
+    Push()
+    transalate(paperpos.x,paperpos.y);
+    rectMode(CENTER)
+    
+
+    )
+}
